6f8153730cf70e714013bb956f5f77f9791eba2c
rellasie <86995253+rellasie@users.noreply.github.com>
2023-06-01 23:05:45 +0700

    add blur handling function for invalid input from user

--- validator.js
+++ validator.js
@@ -3,22 +3,48 @@ function Validator(options) {
 
     var formElement = document.querySelector(options.form)
 
-    console.log(options.rules)
-
     if (formElement) {
-        // console.log(formElement) // test
+        options.rules.forEach(function(rule) {
+            // console.log(rule.selector) // return name, email
+            var inputElement = formElement.querySelector(rule.selector)
+            var errorElement = inputElement.parentElement.querySelector('.form-message')
+            
+            if (inputElement) {
+                inputElement.onblur = function() {
+                    // console.log('blur ' + rule.selector) // test to see what is blurred
+                    // console.log(inputElement.value) // take input value from keyboard
+                    
+                    // value: inputElement.value
+                    // test func: rule.test
+                    var errorMessage = rule.test(inputElement.value)
+                    
+                    if (errorMessage) {
+                        errorElement.innerText = errorMessage
+                        inputElement.parentElement.classList.add('invalid')
+                    } else {
+                        errorElement.innerText = ''
+                        inputElement.parentElement.classList.remove('invalid')
+                    }
 
+                
+                }
+            }
+        })
     }
 }
 
 
 
 // define rules
+/**
+ * 1. When error -> return error message
+ * 2. When valid -> return undefined
+ * */ 
 Validator.isRequired = function (selector) {
     return {
         selector: selector,
-        test: function () {
-
+        test: function (value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này'
         }
     }
 }
